5cd75194f7f04c1c71978d2174824c3a2722b6ed
Victor Domene <victordomene@college.harvard.edu>
2016-05-06 01:07:17 -0400

    More nice debugging prints

--- paxos/receivers/rdtpReceiver.py
+++ paxos/receivers/rdtpReceiver.py
@@ -1,6 +1,6 @@
 """
-This module provides an implementation of the Receiver class using Google's
-RPC Protocol (gRPC).
+This module provides an implementation of the Receiver class using our own
+RDTP Protocol, which we wrote for the Chat Assignment in CS262
 """
 
 import SocketServer
@@ -23,33 +23,63 @@ class rdtpHandler(SocketServer.BaseRequestHandler):
             assert(len(args) >= 2)
 
             method = args[0]
+            
+            if RECEIVER_DEBUG:
+                print "RECEIVER_DEBUG: Received a request with method {} and arguments {}".format(method, args[1:])
 
-            print 'Received rdtp.recv with method:' + method
             if method == 'send_prepare':
-                assert(len(args) == 4)
+                if len(args) != 4:
+                    if RECEIVER_DEBUG:
+                        print "RECEIVER_DEBUG: Received a send_prepare request with incorrect arguments"
+
+                    return
+
                 p, n, proposer = int(args[1]), int(args[2]), args[3]
                 self.server.receiver.handle_prepare(p, n, proposer)
 
-            if method == 'send_promise':
-                assert(len(args) == 6)
+            elif method == 'send_promise':
+                if len(args) != 6:
+                    if RECEIVER_DEBUG:
+                        print "RECEIVER_DEBUG: Received a send_promise request with incorrect arguments"
+
+                    return
+
                 had_previous, p, n, v, acceptor = bool(args[1]), int(args[2]), int(args[3]), int(args[4]), str(args[5])
                 self.server.receiver.handle_promise(had_previous, p, n, v, acceptor)
 
-            if method == 'send_accept_request':
-                assert(len(args) == 5)
+            elif method == 'send_accept_request':
+                if len(args) != 5:
+                    if RECEIVER_DEBUG:
+                        print "RECEIVER_DEBUG: Received a send_accept_request request with incorrect arguments"
+
+                    return
+
                 p, n, v, proposer = int(args[1]), int(args[2]), int(args[3]), str(args[4])
                 self.server.receiver.handle_accept_request(p, n, v, proposer)
 
-            if method == 'send_refuse_proposal':
-                assert(len(args) == 4)
+            elif method == 'send_refuse_proposal':
+                if len(args) != 4:
+                    if RECEIVER_DEBUG:
+                        print "RECEIVER_DEBUG: Received a send_refuse_proposal request with incorrect arguments"
+
+                    return
+
                 p, n, acceptor = int(args[1]), int(args[2]), str(args[3])
                 self.server.receiver.handle_refuse_proposal(p, n, acceptor)
 
-            if method == 'send_accepted':
-                assert(len(args) == 5)
+            elif method == 'send_accepted':
+                if len(args) != 5:
+                    if RECEIVER_DEBUG:
+                        print "RECEIVER_DEBUG: Received a send_accepted request with incorrect arguments"
+
+                    return
+
                 p, n, v, acceptor = int(args[1]), int(args[2]), int(args[3]), str(args[4])
                 self.server.receiver.handle_accepted(p, n, v, acceptor)
 
+            else:
+                if RECEIVER_DEBUG:
+                    print "RECEIVER_DEBUG: Received an unknown request with method {}".format(method)
 
 class rdtpReceiver():
     def __init__(self, proposer, acceptor, learner):
@@ -64,7 +94,8 @@ class rdtpReceiver():
         self.server.server_bind()
         self.server.server_activate()
 
-        print 'Started listening on %s:%d' % (host, port)
+        print 'Started listening on {}:{}'.format(host, port)
+
         thread.start_new_thread(self.serve_forever, ())
 
     def serve_forever(self):
@@ -72,13 +103,13 @@ class rdtpReceiver():
 
     def handle_prepare(self, p, n, proposer):
         if RECEIVER_DEBUG:
-            print "PrepareRequest received: p = {}, n = {}, proposer = {}".format(p, n, proposer)
+            print "RECEIVER_DEBUG: PrepareRequest received: p = {}, n = {}, proposer = {}".format(p, n, proposer)
 
         return self.acceptor.handle_prepare(p, n, proposer)
 
     def handle_accept_request(self, p, n, v, proposer):
         if RECEIVER_DEBUG:
-            print "AcceptRequest received: p = {}, n = {}, v = {}, proposer = {}".format(p, n, v, proposer)
+            print "RECEIVER_DEBUG: AcceptRequest received: p = {}, n = {}, v = {}, proposer = {}".format(p, n, v, proposer)
 
         return self.acceptor.handle_accept_request(p, n, v, proposer)
 
@@ -90,18 +121,18 @@ class rdtpReceiver():
             v = None
 
         if RECEIVER_DEBUG:
-            print "PromiseRequest received: p = {}, n = {}, v = {}, acceptor = {}".format(p, n, v, acceptor)
+            print "RECEIVER_DEBUG: PromiseRequest received: p = {}, n = {}, v = {}, acceptor = {}".format(p, n, v, acceptor)
 
         return self.proposer.handle_promise(had_previous, p, n, v, acceptor)
 
     def handle_refuse_promise(self, p, n, acceptor):
         if RECEIVER_DEBUG:
-            print "RefusePromiseRequest received: p = {}, n = {}, acceptor = {}".format(p, n, acceptor)
+            print "RECEIVER_DEBUG: RefusePromiseRequest received: p = {}, n = {}, acceptor = {}".format(p, n, acceptor)
 
         return self.proposer.handle_refuse_promise(p, n, acceptor)
 
     def handle_accepted(self, p, n, v, acceptor):
         if RECEIVER_DEBUG:
-            print "AcceptedRequest received: p = {}, n = {}, v = {}, acceptor = {}".format(p, n, v, acceptor)
+            print "RECEIVER_DEBUG: AcceptedRequest received: p = {}, n = {}, v = {}, acceptor = {}".format(p, n, v, acceptor)
 
         return self.learner.handle_accepted(p, n, v, acceptor)
